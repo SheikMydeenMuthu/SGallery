72fb773ddb5cee94e8113cd240b6b04235bb4938
Sheik Muthu <Sheik.Muthu@sisystems.com>
2020-03-20 12:00:55 +0530

    Add project files.

---
+++ SGallery/SGallery/Model/ImageList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace SGallery.Model
+{
+    [DataContract]
+    public class ImageList
+    {
+        [DataMember(Name = "photos")]
+        public List<string> Photos = null;
+    }
+
+}
---
+++ SGallery/SGallery/ViewModel/GalleryViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGallery.ViewModel
+{
+    class GalleryViewModel
+    {
+    }
+}
